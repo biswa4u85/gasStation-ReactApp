cef142c635dfb43f01aee03178b94fa9ea2a8a2e
Rakesh Biswal <rakesh@techlift.in>
2018-05-19 16:22:53 +0530

    Design changes

--- src/Main.js
+++ src/Main.js
@@ -74,7 +74,7 @@ class Main extends Component {
                                 <FormGroup>
                                     <Input className="defaultInput" type="text" name="sale" value={this.state.sale} onChange={this.handleChange.bind(this)} placeholder="Sale" />
                                 </FormGroup>
-                                <Button onClick={this.handleSave} className="defaultButt">Print</Button>
+                                <Button onClick={this.handleSave} className="defaultButt" >Print</Button>
                                 <Button type="button" onClick={this.handleReset.bind(this)} className="defaultButt">Reset</Button>
                             </Form>
                         </div>
@@ -114,20 +114,49 @@ class Main extends Component {
                                         <th>Action</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    {
-                                        this.props.allPostsQuery.listGalons.items.map((item, i) => <tr key={i}>
-                                            <td>{item.date}</td>
-                                            <td>{item.invoice}</td>
-                                            <td>{item.galons}</td>
-                                            <td>{item.price}</td>
-                                            <td>{item.total}</td>
-                                            <td><button className="buttBg">Print</button></td>
-                                        </tr>
-                                        )
-                                    }
-                                </tbody>
-                            </table>
+                                </table>
+                                <div className="scrollbar2" id="style-6">
+                                <table className="table">
+                                    <tbody>
+                                        {
+                                            this.props.allPostsQuery.listGalons.items.map((item, i) => <tr key={i}>
+                                                <td>{item.date}</td>
+                                                <td>{item.invoice}</td>
+                                                <td>{item.galons}</td>
+                                                <td>{item.price}</td>
+                                                <td>{item.total}</td>
+                                                <td><button className="buttBg" data-toggle="modal" onClick={this.myModal}>Print</button></td>
+                                            </tr>
+                                            )
+                                        }
+                                    </tbody>
+                                    </table>
+                                </div>
+                                {/* Modal Popup */}
+                                <div class="modal fade" id="myModal">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                    
+                                        {/* <!-- Modal Header --> */}
+                                        <div class="modal-header">
+                                        <h4 class="modal-title">Modal Heading</h4>
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        </div>
+                                        
+                                        {/* <!-- Modal body --> */}
+                                        <div class="modal-body">
+                                        Modal body..
+                                        </div>
+                                        
+                                        {/* <!-- Modal footer --> */}
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                        
+                                    </div>
+                                    </div>
+                                </div>
+                                {/* Modal Popup End */}
                         </div>
                     </div>
                 </div>
--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import logo from './images/logo.png'
 
 class Nav extends Component {
   render() {
     return (
-      <div className="Header">
-        <div className="logo">Logo</div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/ventas">ventas</Link>
-          </li>
-        </ul>
+      <div className="container">
+        <div className="row">
+        <div className="col-sm-4 logo">            
+            <img src={logo} alt=""/>        
+            </div>
+            <div className="col-sm-4">
+                <ul className="navi">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/ventas">Ventas</Link>
+                  </li>
+                </ul>
+            </div>
+            <div className="col-sm-4">
+                <Link to="/ventas" className="loginTxt">Login</Link>
+            </div>
+        </div>      
       </div>
     );
   }
